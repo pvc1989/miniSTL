4fce5c0d89edf08bbe488efb19ba2148ea0f67f0
Weicheng Pei <weicheng.pei@icloud.com>
2019-03-08 13:45:05 +0800

    Initialize pvc::forward_list.

---
+++ include/forward_list.h
@@ -0,0 +1,6 @@
+// Copyright 2019 Weicheng Pei
+
+#ifndef PVC_FORWARD_LIST_H_
+#define PVC_FORWARD_LIST_H_
+
+#endif  // PVC_FORWARD_LIST_H_
---
+++ test/forward_list_test.cc
@@ -0,0 +1,9 @@
+// Copyright 2019 Weicheng Pei
+
+#include "forward_list.h"
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
