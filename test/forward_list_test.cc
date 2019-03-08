7bb30b8790df4c400afad8e12a378cba4993e78f
Weicheng Pei <weicheng.pei@icloud.com>
2019-03-08 13:49:31 +0800

    Initialize tests in ForwardListTest.

--- test/forward_list_test.cc
+++ test/forward_list_test.cc
@@ -3,6 +3,34 @@
 #include "forward_list.h"
 #include "gtest/gtest.h"
 
+class ForwardListTest : public ::testing::Test {
+ protected:
+};
+
+TEST_F(ForwardListTest, Empty) {
+  FAIL();
+}
+
+TEST_F(ForwardListTest, EmplaceFront) {
+  FAIL();
+}
+
+TEST_F(ForwardListTest, ListInitialize) {
+  FAIL();
+}
+
+TEST_F(ForwardListTest, PopFront) {
+  FAIL();
+}
+
+TEST_F(ForwardListTest, BeginEnd) {
+  FAIL();
+}
+
+TEST_F(ForwardListTest, DoAfter) {
+  FAIL();
+}
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
