aba2ff93fbd1a4b974516d75cf78dcd5edcda0b6
Weicheng Pei <weicheng.pei@icloud.com>
2019-03-08 13:53:58 +0800

    Implement empty() and its test.

--- include/forward_list.h
+++ include/forward_list.h
@@ -3,4 +3,35 @@
 #ifndef PVC_FORWARD_LIST_H_
 #define PVC_FORWARD_LIST_H_
 
+#include <memory>
+#include <utility>
+
+namespace pvc {
+template <class T>
+class forward_list {
+ public:
+  using value_type = T;
+  using size_type = std::size_t;
+  using reference = value_type&;
+
+ private:
+  struct Node {
+    value_type value;
+    std::unique_ptr<Node> uptr_next;
+
+    template <class... Args>
+    explicit Node(Args... args) : value(std::forward<Args>(args)...) { }
+  };
+
+   private:
+    Node* ptr_node{ nullptr };
+  };
+
+ public:  // non-modifying methods
+  bool empty() const noexcept { return !uptr_head_.get(); }
+
+ private:
+  std::unique_ptr<Node> uptr_head_{ nullptr };
+};
+}  // namespace pvc
 #endif  // PVC_FORWARD_LIST_H_
--- test/forward_list_test.cc
+++ test/forward_list_test.cc
@@ -1,14 +1,37 @@
 // Copyright 2019 Weicheng Pei
 
 #include "forward_list.h"
+
+#include <algorithm>
+#include <iostream>
+#include <forward_list>
+
 #include "gtest/gtest.h"
+
+using std::cout;
+using std::endl;
 
 class ForwardListTest : public ::testing::Test {
  protected:
+  // helper class
+  struct Kitten {
+    const int id;
+    explicit Kitten(int id) : id(id) { cout << "Kitten(" << id << ')' << endl; }
+    ~Kitten() { cout << id << " ~Kitten()" << endl; }
+    Kitten(const Kitten&) = default;
+    Kitten& operator=(const Kitten&) = default;
+    Kitten(Kitten&&) noexcept = default;
+    Kitten& operator=(Kitten&&) noexcept = default;
+  };
+  // common data
+  std::forward_list<int> std_list_of_id{ 4, 3, 2, 1 };
+  std::forward_list<Kitten> std_list_of_kitten;
+  pvc::forward_list<Kitten> pvc_list_of_kitten;
 };
 
 TEST_F(ForwardListTest, Empty) {
   FAIL();
+  EXPECT_EQ(pvc_list_of_kitten.empty(), std_list_of_kitten.empty());
 }
 
 TEST_F(ForwardListTest, EmplaceFront) {
