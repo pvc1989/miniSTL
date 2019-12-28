71c7312a222e78a252c1ea697b938baa055f89c7
buaaymh <yangminghao@buaa.edu.cn>
2019-12-28 20:30:32 +0800

    Implement `pvc::vector` and its test.

---
+++ include/vector.h
@@ -0,0 +1,239 @@
+// Copyright 2019 Weicheng Pei
+
+#ifndef PVC_VECTOR_H_
+#define PVC_VECTOR_H_
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <memory>
+#include <stdexcept>
+
+#include "iterator.h"
+#include "utility.h"
+
+namespace pvc {
+
+template <class T>
+class vector {
+ public:
+  using size_type = std::size_t;
+  using reference = T&;
+  using const_reference = const T&;
+  using pointer = T*;
+  using const_pointer = const T*;
+
+ public:
+  // construction
+  vector() : capacity_(0), size_(0), array_(new T[0]) {}
+  explicit vector(size_type count) : capacity_(count), size_(count),
+                                     array_(new T[count]) {}
+  explicit vector(size_type count, const T& value) : capacity_(count),
+                                                     size_(count),
+                                                     array_(new T[count]) {
+    for (int i = 0; i < size_; i++) {
+      array_[i] = value;
+    }
+  }
+  vector(std::initializer_list<T> init) {
+    capacity_ = init.end() - init.begin();
+    size_ = capacity_;
+    array_ = std::make_unique<T[]>(size_);
+    auto iter = init.begin();
+    for (int i = 0; i < size_; i++) {
+      array_[i] = *iter;
+      iter++;
+    }
+  }
+  vector& operator=(std::initializer_list<T> init) {
+    size_ = init.end() - init.begin();
+    capacity_ = std::max(size_, capacity_);
+    array_ = std::make_unique<T[]>(size_);
+    auto iter = init.begin();
+    for (int i = 0; i < size_; i++) {
+      array_[i] = *iter;
+      iter++;
+    }
+    return *this;
+  }
+  // destruction
+  ~vector() noexcept { clear(); }
+  // copy operations:
+  vector(const vector& that) { *this = that; }
+  vector& operator=(const vector& that) {
+    if (this != &that) {
+      clear();
+      capacity_ = that.capacity();
+      size_ = that.size();
+      array_ = std::make_unique<T[]>(size_);
+      for (int i = 0; i < size_; i++) {
+        array_[i] = that[i];
+      }
+    }
+    return *this;
+  }
+  // move operations:
+  vector(vector&& that) { *this = std::move(that); }
+  vector& operator=(vector&& that) {
+    if (this != &that) {
+      clear();
+      capacity_ = that.capacity();
+      size_ = that.size();
+      array_.swap(that.array_);
+    }
+    return *this;
+  }
+
+ private:
+  size_type capacity_;
+  size_type size_;
+  std::unique_ptr<T[]> array_;
+
+ public:
+  // iterator and related methods
+  using iterator = pointer;
+
+  iterator begin() noexcept { return array_.get(); };
+  iterator end() noexcept { return array_.get()+size_; }
+
+  using const_iterator = const_pointer;
+
+  const_iterator cbegin() const noexcept { return array_.get(); };
+  const_iterator cend() const noexcept { return array_.get()+size_; }
+
+  const_iterator begin() const noexcept { return cbegin(); };
+  const_iterator end() const noexcept { return cend(); }
+ 
+  // non-modifying methods
+  bool empty() const noexcept {
+    // return size_ == 0;
+    return begin() == end();
+  }
+  reference front() { return array_[0]; }
+  reference back() { return array_[size_ - 1]; }
+  size_type size() { return size_; }
+  size_type capacity() { return capacity_; }
+  T& operator[] (size_type pos) {
+    return array_[pos];
+  }
+  const T& operator[] (size_type pos) const {
+    return array_[pos];
+  }
+  T& at(size_type pos) {
+    if (pos >= size_) {
+      std::out_of_range("Out of range!");
+    }
+    return array_[pos];
+  }
+  const T& at(size_type pos) const{
+    if (pos >= size_) {
+      std::out_of_range("Out of range!");
+    }
+    return array_[pos];
+  }
+
+  // modifying methods
+  void resize(size_type count, const T& value) {
+    if (count > capacity_) {
+      capacity_ = count;
+      auto new_array_ = std::make_unique<T[]>(count);
+      for (int i = 0; i < size_; i++) {
+        std::swap(new_array_[i], array_[i]);
+      }
+      for (int i = size_; i < count; i++) {
+        new_array_[i] = value;
+      }
+      array_.swap(new_array_);
+    } else if (count > size_) {
+      for (int i = size_; i < count; i++) {
+        array_[i] = value;
+      }
+    }
+    size_ = count;
+  }
+  void resize(size_type count) {
+    resize(count, T());
+  }
+  template <class... Args>
+  void emplace_back(Args&&... args) {
+    if (size() == capacity()) {
+      enlarge();
+    }
+    assert(size() < capacity());
+    array_[size_++] = T(std::forward<Args>(args)...);
+  }
+  void push_back(const T& value) {
+    if (size() == capacity()) {
+      enlarge();
+    }
+    assert(size() < capacity());
+    array_[size_++] = value;
+  }
+  void push_back(T&& value) {
+    if (size() == capacity()) {
+      enlarge();
+    }
+    assert(size() < capacity());
+    array_[size_++] = std::move(value);
+  }
+  void pop_back() noexcept {
+    size_--;
+    if (size_ > 0 and size_ <= capacity_/4) {
+      shrink();
+    }
+  }
+  void clear() noexcept {
+    while(not empty()) {
+      pop_back();
+    }
+  }
+  void swap(vector& other) {
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+    array_.swap(other.array_);
+  }
+
+ private:
+  void enlarge() {
+    if (size_ == 0) {
+      capacity_ = 1;
+    } else {
+      capacity_ = size_ * 2;
+    }
+    auto new_array_ = std::make_unique<T[]>(capacity_);
+    for (int i = 0; i < size_; i++) {
+      std::swap(new_array_[i], array_[i]);
+    }
+    array_.swap(new_array_);
+  }
+  void shrink() {
+    capacity_ /= 2;
+    auto new_array_ = std::make_unique<T[]>(capacity_);
+    for (int i = 0; i < size_; i++) {
+      std::swap(new_array_[i], array_[i]);
+    }
+    array_.swap(new_array_);
+  }
+};
+
+template <class T>
+bool operator==(const vector<T>& lhs,
+                const vector<T>& rhs) noexcept {
+  auto iter = lhs.begin();
+  const auto iend = lhs.end();
+  for (const auto& x : rhs) {
+    if (iter == iend || *iter != x) { return false; }
+    else { ++iter; }
+  }
+  if (iter == iend) { return true; }
+  else { return false; }  
+}
+template <class T>
+bool operator!=(const vector<T>& lhs,
+                const vector<T>& rhs) noexcept {
+  return not (lhs == rhs);                 
+}
+
+}  // namespace pvc
+
+#endif  // PVC_vector_H_
---
+++ test/vector_test.cc
@@ -0,0 +1,221 @@
+// Copyright 2019 Weicheng Pei
+
+#include "vector.h"
+
+#include <algorithm>
+#include <chrono>
+#include <forward_list>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+class VectorTest : public ::testing::Test {
+ protected:
+  // helper class
+  struct Kitten {
+    int id;
+    explicit Kitten(int id) : id(id) { }
+    Kitten() = default;
+    ~Kitten() noexcept = default;
+    Kitten(const Kitten&) = default;
+    Kitten& operator=(const Kitten&) = default;
+    Kitten(Kitten&&) noexcept = default;
+    Kitten& operator=(Kitten&&) noexcept = default;
+    bool operator==(const Kitten& that) const { return id == that.id; }
+    bool operator!=(const Kitten& that) const { return id != that.id; }
+  };
+  // common data
+  std::forward_list<int> std_vector_of_id{ 4, 3, 2, 1 };
+  std::vector<Kitten> std_vector_of_kitten;
+  pvc::vector<Kitten> pvc_vector_of_kitten;
+};
+
+TEST_F(VectorTest, Construction_1) {
+  pvc_vector_of_kitten = pvc::vector<Kitten>(4);
+  std_vector_of_kitten = std::vector<Kitten>(4);
+  EXPECT_EQ(pvc_vector_of_kitten.size(), std_vector_of_kitten.size());
+  EXPECT_EQ(pvc_vector_of_kitten.capacity(), std_vector_of_kitten.capacity());
+  auto pvc_vector = pvc::vector<Kitten>(4);
+  auto std_vector = std::vector<Kitten>(4);
+  EXPECT_EQ(pvc_vector.size(), std_vector.size());
+  EXPECT_EQ(pvc_vector.capacity(), std_vector.capacity());
+}
+
+TEST_F(VectorTest, Construction_2) {
+  auto kitten = Kitten(9);
+  auto size = 4;
+  pvc_vector_of_kitten = pvc::vector<Kitten>(size, kitten);
+  std_vector_of_kitten = std::vector<Kitten>(size, kitten);
+  EXPECT_EQ(pvc_vector_of_kitten.size(), std_vector_of_kitten.size());
+  EXPECT_EQ(pvc_vector_of_kitten.capacity(), std_vector_of_kitten.capacity());
+  auto pvc_vector = pvc::vector<Kitten>(size, kitten);
+  auto std_vector = std::vector<Kitten>(size, kitten);
+  EXPECT_EQ(pvc_vector.size(), std_vector.size());
+  for (int i = 0; i < size; i++) {
+    EXPECT_EQ(pvc_vector[i], std_vector[i]);
+  }
+}
+
+TEST_F(VectorTest, Construction_3) {
+  auto list = {1, 2, 3, 5, 6};
+  auto pvc_vector = pvc::vector<int>(list);
+  auto std_vector = std::vector<int>(list);
+  EXPECT_EQ(pvc_vector.size(), std_vector.size());
+  for (int i = 0; i < 5; i++) {
+    EXPECT_EQ(pvc_vector.at(i), std_vector.at(i));
+  }
+  list = {2, 4};
+  pvc_vector = list;
+  std_vector = list;
+  EXPECT_EQ(pvc_vector.size(), std_vector.size());
+  EXPECT_EQ(pvc_vector.capacity(), std_vector.capacity());
+}
+
+TEST_F(VectorTest, Empty) {
+  EXPECT_EQ(pvc_vector_of_kitten.empty(), std_vector_of_kitten.empty());
+}
+
+TEST_F(VectorTest, EmplaceBack) {
+  for (auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+  }
+  EXPECT_EQ(pvc_vector_of_kitten.empty(), std_vector_of_kitten.empty());
+}
+
+TEST_F(VectorTest, Resize) {
+  auto kitten = Kitten(9);
+  for (auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+  }
+  std_vector_of_kitten.resize(8, kitten);
+  pvc_vector_of_kitten.resize(8, kitten);
+  EXPECT_EQ(pvc_vector_of_kitten.size(), std_vector_of_kitten.size());
+  EXPECT_EQ(pvc_vector_of_kitten.capacity(), std_vector_of_kitten.capacity());
+  std_vector_of_kitten.resize(2);
+  pvc_vector_of_kitten.resize(2);
+  EXPECT_EQ(pvc_vector_of_kitten.size(), std_vector_of_kitten.size());
+  EXPECT_EQ(pvc_vector_of_kitten.capacity(), std_vector_of_kitten.capacity());
+}
+
+TEST_F(VectorTest, PushBack) {
+  auto kitten = Kitten(9);
+  for (auto& i : std_vector_of_id) {
+    std_vector_of_kitten.push_back(kitten);
+    pvc_vector_of_kitten.push_back(kitten);
+  }
+  EXPECT_EQ(pvc_vector_of_kitten.empty(), std_vector_of_kitten.empty());
+}
+
+TEST_F(VectorTest, Size_and_Capacity) {
+  for (const auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+    EXPECT_EQ(pvc_vector_of_kitten.size(), std_vector_of_kitten.size());
+    EXPECT_EQ(pvc_vector_of_kitten.capacity(), std_vector_of_kitten.capacity());
+  }
+}
+
+TEST_F(VectorTest, Front) {
+  for (const auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+    EXPECT_EQ(pvc_vector_of_kitten.front(), std_vector_of_kitten.front());
+  }
+}
+
+TEST_F(VectorTest, Back) {
+  for (const auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+    EXPECT_EQ(pvc_vector_of_kitten.back(), std_vector_of_kitten.back());
+  }
+}
+
+TEST_F(VectorTest, PopBack) {
+  for (const auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+  }
+  while (!std_vector_of_kitten.empty()) {
+    std_vector_of_kitten.pop_back();
+    pvc_vector_of_kitten.pop_back();
+    EXPECT_EQ(pvc_vector_of_kitten.empty(), std_vector_of_kitten.empty());
+  }
+}
+
+TEST_F(VectorTest, At) {
+  int j = 0;
+  for (const auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+    EXPECT_EQ(pvc_vector_of_kitten.at(j), std_vector_of_kitten.at(j));
+    j++;
+  }
+}
+
+TEST_F(VectorTest, operator) {
+  int j = 0;
+  for (const auto& i : std_vector_of_id) {
+    std_vector_of_kitten.emplace_back(i);
+    pvc_vector_of_kitten.emplace_back(i);
+    EXPECT_EQ(pvc_vector_of_kitten[j], std_vector_of_kitten[j]);
+    j++;
+  }
+}
+
+
+TEST_F(VectorTest, Iterator) {
+  for (const auto& i : std_vector_of_id) {
+    pvc_vector_of_kitten.emplace_back(i);
+  }
+  // look for an object in the vector
+  auto iter = std::find(
+      pvc_vector_of_kitten.begin(),
+      pvc_vector_of_kitten.end(),
+      Kitten(2));
+  EXPECT_NE(iter, pvc_vector_of_kitten.end());
+  EXPECT_EQ(*iter, Kitten(2));
+  // look for an object not in the vector
+  iter = std::find(
+      pvc_vector_of_kitten.begin(),
+      pvc_vector_of_kitten.end(),
+      Kitten(-2));
+  EXPECT_EQ(iter, pvc_vector_of_kitten.end());
+}
+
+TEST_F(VectorTest, Equal) {
+  auto new_vector_of_kitten = decltype(pvc_vector_of_kitten)();
+  for (const auto& i : std_vector_of_id) {
+    pvc_vector_of_kitten.emplace_back(i);
+    new_vector_of_kitten.emplace_back(i);
+  }
+  EXPECT_TRUE(pvc_vector_of_kitten == pvc_vector_of_kitten);
+  EXPECT_TRUE(new_vector_of_kitten == pvc_vector_of_kitten);
+  EXPECT_FALSE(pvc_vector_of_kitten != pvc_vector_of_kitten);
+  EXPECT_FALSE(new_vector_of_kitten != pvc_vector_of_kitten);
+}
+
+TEST_F(VectorTest, Swap) {
+  auto list_a = {1, 2, 3, 4};
+  auto list_b = {5, 6};
+  auto a = pvc::vector<int>(list_a);
+  auto b = pvc::vector<int>(list_b);
+  auto size_a = a.size();
+  auto size_b = b.size();
+  auto end_of_a = a.back();
+  auto end_of_b = b.back();
+  a.swap(b);
+  EXPECT_EQ(a.size(), size_b);
+  EXPECT_EQ(a.capacity(), size_b);
+  EXPECT_EQ(a.back(), end_of_b);
+  EXPECT_EQ(b.size(), size_a);
+  EXPECT_EQ(b.capacity(), size_a);
+  EXPECT_EQ(b.back(), end_of_a);
+}
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
